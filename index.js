77cd25b5608a110fbe28839a0fb88f30b5d39cf5
fjand5 <huynhtheluat@gmail.com>
2017-07-19 00:01:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-var express = require("express");
-var app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
-server.listen(3000);
-
-io.on("connection", function(socket){
-
-  console.log("Co nguoi ket noi:" + socket.id);
-
-  socket.on("disconnect", function(){
-    console.log(socket.id + " ngat ket noi!!!!!");
-  });
-
-});
-
-
-app.get("/", function(req, res){
-  res.render("trangchu");
-});
+var http = require("http");
+console.log("khoi dong xon");
+http.createServer(function(req,res){
+	console.log("xin chao");
+}).listen(process.env.PORT || 80);
