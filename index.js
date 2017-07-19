ac5579b877fe350ee788584b6a936aba0faebe5b
fjand5 <huynhtheluat@gmail.com>
2017-07-19 11:53:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require("http");
  console.log("khoi dong xong");
  http.createServer(function(req,res){
  	console.log("xin chao");
-  console.log(process.env.POR);
+  console.log(process.env.PORT);
  }).listen(process.env.PORT);
