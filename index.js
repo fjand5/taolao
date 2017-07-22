2a44d49b7bea2ddd30133e08d1193efec4674c86
fjand5 <huynhtheluat@gmail.com>
2017-07-22 11:06:44 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
- console.log("khoi dong xong");
- http.createServer(function(req,res){
- 	console.log("xin chao");
-  console.log(process.env.PORT);
-  res.end("tao lao het suc");
- }).listen(process.env.PORT);
-
